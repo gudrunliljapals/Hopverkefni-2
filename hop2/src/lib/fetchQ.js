fc404a0d453ec866ae9fec5be077d4639a38f1cd
Guðrún Lilja Pálsdóttir <gudrun.lilja.palsdottir@gmail.com>
2025-11-14 19:18:06 +0000

    setja upp fetchQ og byrja á searchQ

---
+++ hop2/src/lib/fetchQ.js
@@ -0,0 +1,31 @@
+import Papa from 'papaparse';
+/**
+ * 
+ * @param {string} database - string containing path to static database file
+ * @returns -
+ */
+
+export async function fetchQ(database) {
+    try {
+        const data = await fetch(database);
+        if (!data.ok) {
+            throw new Error(`Fetching data failed: ${data.status} ${data.statusText}`);
+        }
+        
+        const data_text = await data.text();
+        const data_parsed = Papa.parse(data_text, 
+            {header: false,
+             skipEmptyLines: true
+        });
+        return data_parsed.data;
+
+    } catch (error) {
+        console.warn('Error fetching data:', error);
+        return null;
+    }
+}
+
+
+
+
+        
